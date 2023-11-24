9bb382ea4d7383701894ebe97cff52334544c3ff
Weedshaker <weedshaker@gmail.com>
2023-11-24 12:15:05 +0100

    manifest sub

--- src/es/components/organisms/footer/Footer.js
+++ src/es/components/organisms/footer/Footer.js
@@ -93,7 +93,7 @@ export default class Footer extends Shadow() {
    */
   renderHTML () {
     this.footer = this.root.querySelector(this.cssSelector + ' > footer') || document.createElement('footer')
-    this.footer.innerHTML = '<a href="https://github.com/decentral-ninja" target="_blank">© decentral.ninja / alpha 1.0</a>'
+    this.footer.innerHTML = '<a href="https://github.com/decentral-ninja" target="_blank">© decentral.ninja / alpha 1.1</a>'
     Array.from(this.root.children).forEach(node => {
       if (node === this.footer || node.getAttribute('slot') || node.nodeName === 'STYLE') return false
       this.footer.appendChild(node)
