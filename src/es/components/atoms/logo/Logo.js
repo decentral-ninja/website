d76471f4389828790e10004fbdb20a118ca4d399
Weedshaker <weedshaker@gmail.com>
2024-03-11 14:01:50 +0100

    logo fix

--- src/es/components/atoms/logo/Logo.js
+++ src/es/components/atoms/logo/Logo.js
@@ -66,14 +66,10 @@ export default class Logo extends Shadow(WebWorker()) {
   }
 
   connectedCallback () {
-    this.hidden = true
     const showPromises = []
     if (this.shouldRenderCSS()) showPromises.push(this.renderCSS())
     if (this.shouldRenderHTML()) showPromises.push(this.renderHTML())
-    Promise.all(showPromises).then(() => {
-      this.svgs.forEach(svg => svg.addEventListener('animationiteration', this.animationiterationListener))
-      this.hidden = false
-    })
+    Promise.all(showPromises).then(() => this.svgs.forEach(svg => svg.addEventListener('animationiteration', this.animationiterationListener)))
     this.addEventListener('click', this.clickEventListener)
   }
 
