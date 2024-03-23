1a54c1b2c8bdafa4fc6f3dd8c00476b3df1b5f65
Weedshaker <weedshaker@gmail.com>
2024-03-23 16:34:18 +0100

    sub

--- src/es/Environment.js
+++ src/es/Environment.js
@@ -9,7 +9,7 @@ self.Environment = {
   language: currentScriptUrl.searchParams.get('language') || document.documentElement.getAttribute('lang') || 'en',
   stage: currentScriptUrl.searchParams.get('stage') || document.documentElement.getAttribute('stage') || 'alpha',
   keepAlive: 86400000,
-  version: currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '4.0.7', // https://semver.org/
+  version: currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '4.0.8', // https://semver.org/
   /**
    * Get custom mobile breakpoint
    * @param {{constructor?: string, tagName?: string, namespace?: string}} organism
