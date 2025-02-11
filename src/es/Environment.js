b2c807862004a3a4107d4fccbce9f8760d2b22fe
Weedshaker <weedshaker@gmail.com>
2025-02-12 00:24:15 +0100

    faster

--- src/es/Environment.js
+++ src/es/Environment.js
@@ -10,9 +10,9 @@ self.Environment = {
   roomNamePrefix: 'chat-',
   stage: currentScriptUrl.searchParams.get('stage') || document.documentElement.getAttribute('stage') || 'alpha',
   keepAlive: 86400000,
-  version: currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '4.5.5', // https://semver.org/
+  version: currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '4.5.6', // https://semver.org/
   timestampNamespace: 't_',
-  awarenessEventListenerDelay: 5000, // the delay to react on events like 'yjs-users' or 'yjs-providers'
+  awarenessEventListenerDelay: 1000, // the delay to react on events like 'yjs-users' or 'yjs-providers'
   /**
    * Get custom mobile breakpoint
    * @param {{constructor?: string, tagName?: string, namespace?: string}} organism
