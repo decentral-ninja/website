7c2a6f290deaebedff694b1a89acb350f0ed4918
Weedshaker <weedshaker@gmail.com>
2025-02-12 13:29:16 +0100

    sub

--- src/es/Environment.js
+++ src/es/Environment.js
@@ -10,7 +10,7 @@ self.Environment = {
   roomNamePrefix: 'chat-',
   stage: currentScriptUrl.searchParams.get('stage') || document.documentElement.getAttribute('stage') || 'alpha',
   keepAlive: 86400000,
-  version: currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '4.5.6', // https://semver.org/
+  version: currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '4.5.7', // https://semver.org/
   timestampNamespace: 't_',
   awarenessEventListenerDelay: 1000, // the delay to react on events like 'yjs-users' or 'yjs-providers'
   /**
