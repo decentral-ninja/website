afcd30cfa6a29689edda4f4e3e2e3bd82028ed24
Weedshaker <weedshaker@gmail.com>
2025-09-10 14:55:11 +0200

    sub readme

--- src/es/Environment.js
+++ src/es/Environment.js
@@ -8,7 +8,7 @@ self.Environment = {
   isTestingEnv: location.hostname === 'localhost',
   language: currentScriptUrl.searchParams.get('language') || document.documentElement.getAttribute('lang') || 'en',
   stage: currentScriptUrl.searchParams.get('stage') || document.documentElement.getAttribute('stage') || 'alpha',
-  version: currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '5.0.12', // https://semver.org/
+  version: currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '5.0.13', // https://semver.org/
   roomNamePrefix: 'chat-',
   updateNotificationsAfter: 5000,
   keepAlive: 86400000,
@@ -18,11 +18,11 @@ self.Environment = {
   },
   {
     name: 'websocket',
-    url: 'https://the-decentral-web.loca.lt'
+    url: 'https://the-decentral-web.loca.lt' // TODO: test and then remove this line cause the loca.lt subdomain could be hijacked
   },
   {
     name: 'websocket',
-    url: 'https://decentral-ninja.loca.lt'
+    url: 'https://decentral-ninja.loca.lt' // TODO: test and then remove this line cause the loca.lt subdomain could be hijacked
   }],
   permanentFallbacks: new Map([['wss://the-decentral-web.herokuapp.com', 'https://the-decentral-web.loca.lt']]), // TODO: permanentFallbacks test and then remove this line as long as heroku works
   alternativeWebsiteHosts: ['https://decentralninja.app.runonflux.io/', 'https://decentralninja_8080.app.runonflux.io'],
