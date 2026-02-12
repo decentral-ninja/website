fcb3757fb513d913f6102a8c383cd26822518a3c
Weedshaker <weedshaker@gmail.com>
2026-02-12 21:02:12 +0100

    sub advanced crypto error handling

--- src/es/Helpers.js
+++ src/es/Helpers.js
@@ -1,9 +1,15 @@
 /* global self */
 
 const hexColorMap = new Map()
-export const getHexColor = async (string) => hexColorMap.has(string)
-  ? hexColorMap.get(string)
-  : hexColorMap.set(string, `#${Array.from(new Uint8Array(await self.crypto.subtle.digest('SHA-256', new TextEncoder().encode(string)))).map(b => b.toString(16).padStart(2, '0')).join('').substring(0, 6)}`).get(string)
+export const getHexColor = async (string) => {
+  try {
+    return hexColorMap.has(string)
+      ? hexColorMap.get(string)
+      : hexColorMap.set(string, `#${Array.from(new Uint8Array(await self.crypto.subtle.digest('SHA-256', new TextEncoder().encode(string)))).map(b => b.toString(16).padStart(2, '0')).join('').substring(0, 6)}`).get(string)
+  } catch (error) {
+    return 'var(--color-secondary, var(--color, #ff0000))'
+  }
+}
 
 export const jsonStringifyMapUrlReplacer = (key, value) => {
   if (value instanceof Map) {
