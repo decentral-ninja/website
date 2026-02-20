c2455c8cdbf3c845e0aa5d6b7d15a5bfab9dcbde
Weedshaker <weedshaker@gmail.com>
2026-02-20 20:20:09 +0100

    icon combo improved readme

--- src/es/components/atoms/iconCombinations/IconCombinations.js
+++ src/es/components/atoms/iconCombinations/IconCombinations.js
@@ -61,27 +61,11 @@ export default class IconCombinations extends Shadow() {
       :host > section > *, :host > section > wct-icon-mdx {
         grid-column: 1;
         grid-row: 1;
-        display: block;
+        display: flex;
       }
       :host > section > wct-icon-mdx {
         align-self: center;
         justify-self: center;
-      }
-      :host([keys]) > section > wct-icon-mdx {
-        opacity: 0.6;
-      }
-      :host([icons-count="2"][keys]) > section > wct-icon-mdx:first-of-type {
-        transform: rotate(20deg);
-      }
-      :host([icons-count="2"][keys]) > section > wct-icon-mdx:last-of-type {
-        transform: rotate(-20deg) translate(12%, 11%);
-      }
-      :host([add-key]) > section > wct-icon-mdx:first-of-type {
-        mask: url("data:image/svg+xml,%3Csvg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 297 297'%3E%3Cpath d='M294.061 101.395a9.869 9.869 0 00-6.976-2.887l-88.617.012.013-88.655a9.868 9.868 0 00-2.888-6.977A9.868 9.868 0 00188.616 0l-80.188.012c-5.445.002-9.859 4.415-9.86 9.86l-.016 88.662-88.626.014c-5.446 0-9.86 4.415-9.861 9.86L.051 188.63a9.863 9.863 0 009.865 9.865l88.621-.012-.014 88.653a9.86 9.86 0 009.866 9.865l80.187-.016c5.445 0 9.859-4.415 9.86-9.86l.014-88.658 88.629-.016c5.445 0 9.859-4.415 9.86-9.86l.012-80.22a9.876 9.876 0 00-2.89-6.976z'/%3E%3C/svg%3E") 98% 82% / 50% no-repeat exclude, linear-gradient(#000 0 0)
-      }
-      :host([add-key]) > section > wct-icon-mdx:last-of-type {
-        align-self: end;
-        justify-self: end;
       }
       /* COULD-DO: https://prismic.io/blog/css-animation-examples */
       :host > section > a-loading {
@@ -118,7 +102,40 @@ export default class IconCombinations extends Shadow() {
         --color-yellow: var(--color-disabled);
       }
     `
-    return Promise.resolve()
+    return this.fetchTemplate()
+  }
+
+  /**
+   * fetches the template
+   *
+   * @return {Promise<void>}
+   */
+  fetchTemplate () {
+    const replaces = this.buttonTagName === 'a'
+      ? [{
+          pattern: '([^-]{1})button',
+          flags: 'g',
+          replacement: '$1a'
+        }]
+      : []
+    switch (this.getAttribute('namespace')) {
+      case 'icon-combinations-double-keys-':
+        return this.fetchCSS([{
+          // @ts-ignore
+          path: `${this.importMetaUrl}./double-keys-/double-keys-.css`,
+          namespace: false,
+          replaces
+        }], false)
+      case 'icon-combinations-add-key-':
+        return this.fetchCSS([{
+          // @ts-ignore
+          path: `${this.importMetaUrl}./add-key-/add-key-.css`,
+          namespace: false,
+          replaces
+        }], false)
+      default:
+        return Promise.resolve()
+    }
   }
 
   /**
