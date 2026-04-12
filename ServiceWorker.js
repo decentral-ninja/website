c96439317ea0ca5aa3aa8d08c935a535e2f5fb51
Weedshaker <weedshaker@gmail.com>
2026-04-12 19:34:21 +0200

    sub font fix

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -16,7 +16,7 @@ class ServiceWorker extends NotificationServiceWorker {
     super()
 
     this.name = 'ServiceWorker'
-    this.version = 'v32'
+    this.version = 'v33'
     // KEEP DOING: When version upgrade also update the precache. This is a manual process by clicking through all the routes and dialogs, then enter the following code snippet into the console and copy/paste the result into this.precache:
     // Code: document.body.prepend(Array.from(new Set(self.performance.getEntriesByType('resource').filter(resource => resource.name.includes(location.origin)).map(resource => {let url = resource.name;try{url = new URL(resource.name);url.searchParams.delete('version');url = url.href}catch(error){}return url.replace(location.origin, '.')}).sort((a, b) => a < b ? -1 : a > b ? 1 : 0))).reduce((textarea, curr) => {textarea.value += `'${curr}',\n`;return textarea}, document.createElement('textarea')))
     this.precache = [
@@ -127,7 +127,7 @@ class ServiceWorker extends NotificationServiceWorker {
       './src/es/web-components-toolbox/src/es/components/prototypes/Mutation.js',
       './src/es/web-components-toolbox/src/es/components/prototypes/Shadow.js',
       './src/es/web-components-toolbox/src/es/helpers/Helpers.js',
-      './src/font/Kashima.woff2',
+      './src/font/night-in-tokyo.regular.woff2',
       './src/img/icons/alert-triangle.svg',
       './src/img/icons/android-icon-144x144.png',
       './src/img/icons/arrow-back-up.svg',
