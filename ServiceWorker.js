50f17fbaf2d5562870517880a2758730bd366253
Weedshaker <weedshaker@gmail.com>
2026-05-03 16:14:49 +0200

    sub

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -17,7 +17,7 @@ class ServiceWorker extends NotificationServiceWorker {
     super()
 
     this.name = 'ServiceWorker'
-    this.version = 'v56'
+    this.version = 'v57'
     this.decentralNinjaOrigin = 'https://decentral.ninja'
     if (location.hostname === 'localhost' || location.origin === this.decentralNinjaOrigin) {
       this.decentralNinjaRequestsAvailable = false
