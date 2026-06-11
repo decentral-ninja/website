3e4e5ca79e451a9de61da339b5f4e1c67227207f
Weedshaker <weedshaker@gmail.com>
2026-06-11 20:56:36 +0200

    sub

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -18,7 +18,7 @@ class ServiceWorker extends IpfsServiceWorker(NotificationServiceWorker()) {
     super()
 
     this.name = 'ServiceWorker'
-    this.version = 'v118'
+    this.version = 'v119'
     this.decentralNinjaOrigin = 'https://decentral.ninja'
     if (location.hostname === 'localhost' || location.origin === this.decentralNinjaOrigin) {
       this.decentralNinjaRequestsAvailable = false
