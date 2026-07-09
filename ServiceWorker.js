72856078c1579fb456367d991b107286c39a9a84
Weedshaker <weedshaker@starsystems.ch>
2026-07-09 20:15:08 +0200

    sub

--- ServiceWorker.js
+++ ServiceWorker.js
@@ -18,7 +18,7 @@ class ServiceWorker extends IpfsServiceWorker(NotificationServiceWorker()) {
     super()
 
     this.name = 'ServiceWorker'
-    this.version = 'v164'
+    this.version = 'v165'
     this.decentralNinjaOrigin = 'https://decentral.ninja'
     if (location.hostname === 'localhost' || location.origin === this.decentralNinjaOrigin) {
       this.decentralNinjaRequestsAvailable = false
