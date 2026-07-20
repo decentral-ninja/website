ff7511e3fd84f37435b96622bd224a2d1bbeee4a
Weedshaker <weedshaker@starsystems.ch>
2026-07-20 22:51:54 +0200

    fix purify regex sub

--- src/es/Environment.js
+++ src/es/Environment.js
@@ -8,7 +8,7 @@ self.Environment = {
   isTestingEnv: location.hostname === 'localhost',
   language: currentScriptUrl.searchParams.get('language') || document.documentElement.getAttribute('lang') || 'en',
   stage: currentScriptUrl.searchParams.get('stage') || document.documentElement.getAttribute('stage') || '',
-  version: `version=${currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '2.0.0'}`, // https://semver.org/
+  version: `version=${currentScriptUrl.searchParams.get('version') || document.documentElement.getAttribute('version') || '2.0.1'}`, // https://semver.org/
   roomNamePrefix: 'chat-',
   updateNotificationsAfter: 5000,
   updateProviderPerformanceAfter: 120000,
@@ -111,7 +111,11 @@ if (typeof self.trustedTypes?.createPolicy === 'function' && !self.trustedTypes.
     // complex look ahead: (?:"[^"]*"|'[^']*'|[^'">])* to fix what a selector like [^>]* would not catch: <img src='x>yz' onerror=alert('XSS')>
     createHTML: sanitizer && typeof Document.parseHTML === 'function'
       ? string => Document.parseHTML(string, { sanitizer }).body.innerHTML
-      : string => string.replace(/<[a-zA-Z][a-zA-Z0-9._-]*(?=(?:"[^"]*"|'[^']*'|[^'">])*(?:\bon[a-z]{2,}\s*=|=\s*["']?\s*javascript\s*:))(?:"[^"]*"|'[^']*'|[^'">])*>/gi, ''), // eslint-disable-line
+      : string => string.replace(/<[a-zA-Z][a-zA-Z0-9._-]*(?=(?:"[^"]*"|'[^']*'|[^'">])*(?:(\bon[a-z]{2,})\s*=|(?:href|src|action|formaction|poster|data|xlink:href)\s*=\s*["']?([^"'<>]*)(?::|&colon;?|&#(?:x0*3a|0*58);?)))(?:"[^"]*"|'[^']*'|[^'">])*>/gi, (match, captureAttributeName, captureAttributeValue) => {
+        if (captureAttributeName) return '' // included an attribute eg. onerror=
+        if (captureAttributeValue && /(javascript|vbscript|data:|&(?:#[0-9]+|#x[0-9a-f]+|[a-z][a-z0-9]+);?)/i.test(captureAttributeValue.replace(/[\u0000-\u0020]/g, ''))) return '' // included an attribute value eg. ="javascript:"
+        return match
+      }),
     createScriptURL: string => string, // unsafe but including webworker's, service workers, etc. is okay
     createScript: string => string // unsafe but eval at css templates is okay
   })
