fe03083eab59d1053ea57dbb419f5830e5b8d578
Weedshaker <weedshaker@starsystems.ch>
2026-07-22 21:32:47 +0200

    ipfs update

--- src/es/components/pages/Decentralization.js
+++ src/es/components/pages/Decentralization.js
@@ -107,7 +107,7 @@ to raw TCP or UDP sockets
                 <ul>
                   <li>decentral-ninja/website - <a href="https://github.com/decentral-ninja/website" target="_blank">github</a></li>
                 </ul>
-                <p>customize your own DCN web. Users from your origin (example https://your-website.com) will still be able to connect with users on origin https://decentral.ninja . Note: Every origin creates a separate session and for that is isolated within the browser. Try <a href="https://dcn-web.hostlocal.app/ipfs/QmSxFi1aLBrGr141KbsswunyYUD9L936ou8kSYEGTw5E26" target=_blank>DCN hosted at ipfs</a>... on any public ipfs gateway: <a href="https://ipfs.oversas.org/ipfs/QmSxFi1aLBrGr141KbsswunyYUD9L936ou8kSYEGTw5E26" target=_blank>https://ipfs.oversas.org</a>!</p>
+                <p>customize your own DCN web. Users from your origin (example https://your-website.com) will still be able to connect with users on origin https://decentral.ninja . Note: Every origin creates a separate session and for that is isolated within the browser. Try <a href="https://dcn-web.hostlocal.app/ipfs/QmUcYNResv37Cr3gK2jZ4LKVVrv32dU4mdjRTxbx8z8Rao" target=_blank>DCN hosted at ipfs</a>... on any public ipfs gateway: <a href="https://ipfs.oversas.org/ipfs/QmUcYNResv37Cr3gK2jZ4LKVVrv32dU4mdjRTxbx8z8Rao" target=_blank>https://ipfs.oversas.org</a>!</p>
                 <hr>
                 <h4 style="text-decoration: underline;">hosting - CRDT connection providers:</h4>
                 <ul>
